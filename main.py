e252b455d714e697e0b13d3e8c62490906ccf1c1
Art Beckett <89126163+Bear1986@users.noreply.github.com>
2021-10-02 07:59:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+student_grades = [9.1, 8.8, 7.9]
+
+mysum = sum(student_grades)
+length = len(student_grades)
+mean = mysum / length
+
+print(mean)
+
+x = [33, 66, 99]
+y = max(x)
+print(y)
+
+student_grades = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
+a = student_grades.count(10)
+print(a)
+
+username = "Python3"
+a = username.title()
+print(a)
